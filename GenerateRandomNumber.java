af57818ae94c04a4d401b20d0421c969d291bba8
Kucharlapati Tarun Chaitanya Varma <30628531+tarunchaitu@users.noreply.github.com>
2020-07-05 13:45:32 +0530

    Update GenerateRandomNumber.java

--- GenerateRandomNumber.java
+++ GenerateRandomNumber.java
@@ -1,12 +1,13 @@
 import java.util.*;
 class GenerateRandomNumber {
    public static void main(String[] args) {
-      int counter;
+      int counter,a;
+      a = Integer.parseInt(args[0]);
       Random rnum = new Random();
       System.out.println("Random Numbers:");
       System.out.println("********ooo*****");
       for (counter = 1; counter <= 9; counter++) {
-         System.out.println(rnum.nextInt(1000));
+         System.out.println(rnum.nextInt(a));
       }
    }
 }
